acfedab2f70cf0e63748b4ba9588a1972f9c6fac
Long C Lam <31355535+eemperor@users.noreply.github.com>
2022-11-17 21:48:01 +0000

    Combines terraform and terragrunt downloads with providers

--- create-mirror.sh
+++ create-mirror.sh
@@ -6,16 +6,36 @@ set -e -u -o pipefail
 
 settings_file=$1
 
-platform="linux_amd64"
-# platform="darwin_amd64" # if you want to test from a mac
 mirror_dir="./mirror"
 working_dir=$(pwd)
-mkdir -p ${mirror_dir}
+mkdir -p ${mirror_dir}/terraform ${mirror_dir}/terragrunt
 
-download_provider(){
+download_terraform(){
+  local terraform_version=$1
+  local platform=$2
+  local file_name=terraform_${terraform_version}_${platform}.zip
+  local url=https://releases.hashicorp.com/terraform/${terraform_version}/terraform_${terraform_version}_${platform}.zip
+
+  echo "Downloading Terraform version ${terraform_version} for ${platform}"
+  curl -Lo "${working_dir}/${mirror_dir}/terraform/${file_name}" "${url}"
+}
+
+download_terragrunt(){
+  local terragrunt_version=$1
+  local platform=$2
+  local file_extension=$(echo ${platform} | sed -r "s/windows_(.*)/${platform}.exe/")
+  local file_name=terragrunt_${terragrunt_version}_${file_extension}
+  local url=https://github.com/gruntwork-io/terragrunt/releases/download/v${terragrunt_version}/terragrunt_${file_extension}
+
+  echo "Downloading Terragrunt version ${terragrunt_version} for ${platform}"
+  curl -Lo "${working_dir}/${mirror_dir}/terragrunt/${file_name}" "${url}"
+}
+
+download_providers(){
   local provider_namespace=$1
   local provider_name=$2
   local provider_version=$3
+  local platform=$4
 
   echo "Downloading Terraform Provider ${provider_namespace}/${provider_name}:${provider_version}"
   cat > main.tf << EOF
@@ -33,16 +53,42 @@ EOF
 }
 
 settings_json=$(cat ${settings_file})
+terraform_versions=$(echo ${settings_json} | jq '[.terraform[]]')
+terragrunt_versions=$(echo ${settings_json} | jq '[.terragrunt[]]')
+platforms=$(echo ${settings_json} | jq '[.platforms[]]')
 providers=$(echo ${settings_json} | jq '[ .providers[] ]')
 provider_names=$(echo ${settings_json} | jq '[ .providers[].name ]')
 
 echo
 echo "Mirror Settings:"
+echo "  Terraform Versions:     ${terraform_versions}"
+echo "  Platforms:         ${platforms}"
 echo "  Providers:         ${provider_names}"
 echo
 
-echo "Downloading Providers Locally"
 cd ${mirror_dir}
+
+echo "Downloading Terraform Versions Locally"
+
+for version in $(echo ${terraform_versions} | jq -r '.[]'); do
+  for platform in $(echo ${platforms} | jq -r '.[]'); do
+      #download_terraform $version $platform
+      echo TERRAFORM
+  done
+done
+
+echo "Downloading Terragrunt Versions Locally"
+
+echo ${terragrunt_versions}
+
+for version in $(echo ${terragrunt_versions} | jq -r '.[]'); do
+  for platform in $(echo ${platforms} | jq -r '.[]'); do
+      download_terragrunt $version $platform
+  done
+done
+
+echo "Downloading Providers Locally"
+
 for row in $(echo ${providers} | jq -r '.[] | [.namespace, .name, .versions] | @base64'); do
   _namespace() {
     echo ${row} | base64 --decode | jq -r .[0]
@@ -55,11 +101,13 @@ for row in $(echo ${providers} | jq -r '.[] | [.namespace, .name, .versions] | @
   }
 
   # echo $(_name) $(_versions)
-  for version in $(_versions | jq -r .[]); do
+  for version in $(_versions | jq -r '.[]'); do
     ns=$(_namespace)
     n=$(_name)
-    v=${version}
 
-    download_provider $ns $n $v
+    for platform in $(echo ${platforms} | jq -r '.[]'); do
+      #download_providers $ns $n $version $platform
+      echo PROVIDERS
+    done
   done
 done
